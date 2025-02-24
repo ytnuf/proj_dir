d45e8685edadee3c26d47aeff8255a357979e30c
ytnuf <161308826+ytnuf@users.noreply.github.com>
2025-02-24 15:31:17 +0000

    Create empty library

---
+++ include/dirs/linux.hpp
@@ -0,0 +1,10 @@
+
+#ifndef HPP_PROJDIRS_DIRS_LINUX_
+#define HPP_PROJDIRS_DIRS_LINUX_
+
+#ifdef __linux__
+
+
+#endif // ifdef __linux__
+
+#endif // ifndef HPP_PROJDIRS_DIRS_LINUX_
---
+++ src/linux.cpp
@@ -0,0 +1,6 @@
+
+#ifdef __linux__
+
+#include <dirs/linux.hpp>
+
+#endif // ifdef __linux__
